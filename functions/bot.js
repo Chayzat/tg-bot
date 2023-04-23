b5c3a04b66810fa430bc0006927e4f1bbf9cdfa3
chayzat <chayzat.oorzhak.2001@mail.ru>
2023-04-23 14:54:27 +0300

    feat: try to comvert

--- functions/bot.js
+++ functions/bot.js
@@ -7,59 +7,49 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => ctx.reply("Welcome"));
 bot.help((ctx) => ctx.reply("Send me a link to the video on YouTube"));
 
-// const getID = (url) => {
-//   const regx = url.match(/v=[\w\-\_]{11}|e\/[\w\-\_]{11}/gi);
-//   return regx.join("").substring(2);
-// };
+const getID = (url) => {
+  const regx = url.match(/v=[\w\-\_]{11}|e\/[\w\-\_]{11}/gi);
+  return regx.join("").substring(2);
+};
 
-// bot.on("message", async (ctx) => {
-//   const url = ctx.message.text;
+bot.on("message", async (ctx) => {
+  const url = ctx.message.text;
 
-//   if (ctx.message.entities === undefined) {
-//     ctx.reply("👍");
-//   } else if (
-//     (ctx.message.entities[0].type === "url" &&
-//       url.includes("https://youtu.be/")) ||
-//     url.includes("https://www.youtube.com/watch?v=")
-//   ) {
-//     const options = {
-//       method: "GET",
-//       url: `https://youtube-mp36.p.rapidapi.com/dl?id=${getID(url)}`,
-//       headers: {
-//         "x-rapidapi-key": process.env.API_KEY,
-//         "x-rapidapi-host": process.env.API_HOST,
-//       },
-//     };
+  if (ctx.message.entities === undefined) {
+    ctx.reply("👍");
+  } else if (
+    (ctx.message.entities[0].type === "url" &&
+      url.includes("https://youtu.be/")) ||
+    url.includes("https://www.youtube.com/watch?v=")
+  ) {
+    const options = {
+      method: "GET",
+      url: `https://youtube-mp36.p.rapidapi.com/dl?id=${getID(url)}`,
+      headers: {
+        "x-rapidapi-key": process.env.API_KEY,
+        "x-rapidapi-host": process.env.API_HOST,
+      },
+    };
 
-//     await axios
-//       .request(options)
-//       .then((response) => ctx.replyWithAudio(response.data.link))
-//       .catch((error) => {
-//         ctx.reply("Возникла ошибка. Повторите попытку");
-//         console.error(error);
-//       });
-//   } else if (
-//     !url.includes("https://youtu.be") ||
-//     !url.includes("https://www.youtube.com")
-//   ) {
-//     ctx.reply("😒");
-//   }
-// });
+    await axios
+      .request(options)
+      .then((response) => ctx.replyWithAudio(response.data.link))
+      .catch((error) => {
+        ctx.reply("Возникла ошибка. Повторите попытку");
+        console.error(error);
+      });
+  } else if (
+    !url.includes("https://youtu.be") ||
+    !url.includes("https://www.youtube.com")
+  ) {
+    ctx.reply("😒");
+  }
+});
 
 // bot.launch();
 
 // process.once("SIGINT", () => bot.stop("SIGINT"));
 // process.once("SIGTERM", () => bot.stop("SIGTERM"));
-
-// bot.start(ctx => {
-//   console.log("Received /start command")
-//   try {
-//       ctx.reply("Congrats! You've connected to Netlify!")
-//   } catch (e) {
-//       console.error("error in start action:", e)
-//       ctx.reply("Error occured")
-//   }
-// })
 
 bot.command('thumbsup', async (ctx) => {
     try {
