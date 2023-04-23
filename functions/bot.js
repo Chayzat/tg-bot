ab8cc7714e5b7d387f5b3e2d937fdff69764eeeb
chayzat <chayzat.oorzhak.2001@mail.ru>
2023-04-23 15:54:39 +0300

    refactor: change reply

--- functions/bot.js
+++ functions/bot.js
@@ -34,9 +34,8 @@ bot.on("message", async (ctx) => {
     await axios
       .request(options)
       // .then((response) => ctx.replyWithAudio(response.data.link))
-      .then((response) => ctx.replyWithAudio(response.data.link))
+      .then((response) => ctx.reply(response.data.link))
       .catch((error) => {
-        ctx.reply(error)
         ctx.reply("Возникла ошибка. Повторите попытку");
         console.error(error);
       });
